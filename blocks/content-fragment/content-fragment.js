e46138c077483dd78c9dc9ab57c5fbcb10d8696d
Goura158 <130237259+Goura158@users.noreply.github.com>
2025-05-27 10:12:49 -0400

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -25,25 +25,16 @@ export async function loadFragment(path) {
     // eslint-disable-next-line no-param-reassign
     // path = path.replace(/(\.plain)?\.html/, '');
     const path1 = `${aemauthorurl}/graphql/execute.json/universal-editor-standard-site/text`;
-    const resp = await fetch(`${path1}`);
+    const resp = await fetch(`${path1}`)
+      .then((response) => response.json())
+      .then((contentfragment) => {
+        let offer = '';
+        if (contentfragment.data) {
+          offer = contentfragment.data[Object.keys(contentfragment.data)[0]].item;
+        }
+        return offer;
+      });
     console.log(resp);
-    if (resp.ok) {
-      const main = document.createElement('main');
-      main.innerHTML = await resp.text();
-
-      // reset base path for media to fragment base
-      const resetAttributeBase = (tag, attr) => {
-        main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
-          elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
-        });
-      };
-      resetAttributeBase('img', 'src');
-      resetAttributeBase('source', 'srcset');
-
-      decorateMain(main);
-      await loadSections(main);
-      return main;
-    }
   }
   return null;
 }
