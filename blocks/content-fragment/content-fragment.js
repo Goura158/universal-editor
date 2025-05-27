58dfc080d46892d81ceb534c27cf76ca3715148b
Goura158 <130237259+Goura158@users.noreply.github.com>
2025-05-27 10:25:45 -0400

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -31,8 +31,7 @@ export async function loadFragment(path) {
         if (contentfragment.data) {
           textcf = contentfragment.data[Object.keys(contentfragment.data)[0]].item;
         }
-        return textcf;     
-      });
+        return textcf;});
     console.log(resp);
   }
   return null;
