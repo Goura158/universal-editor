e11243255387bb0d4c593ffd3a379a5c60815230
Goura158 <130237259+Goura158@users.noreply.github.com>
2025-05-28 16:23:41 -0400

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -41,7 +41,7 @@ export async function loadFragment(path) {
 export default async function decorate(block) {
 	const graphqlpath = `https://author-p14733-e1160558.adobeaemcloud.com/graphql/execute.json/universal-editor-standard-site/text`;
 	const graphqlpathresult = await loadFragment(`graphqlpath`);
-	// console.log(graphqlpathresult);
+	console.log(graphqlpathresult);
 	// const path = link ? link.getAttribute('href') : block.textContent.trim();
 	// const cftext = document.createElement('div');
   // while (fragment.title) cftext.append(fragment.title);
