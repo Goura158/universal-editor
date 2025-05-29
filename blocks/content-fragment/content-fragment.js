591cc0bae239a6662e967fdb02bd9e46cafe1654
Goura158 <130237259+Goura158@users.noreply.github.com>
2025-05-29 09:31:20 -0400

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -22,7 +22,7 @@ export default async function decorate(block) {
   const aemauthorurl = getMetadata('authorurl') || '';
   const persistedquery = '/graphql/execute.json/universal-editor-standard-site/text';
   const graphqlpath = `${aemauthorurl}${persistedquery}`;
-  //console.log(graphqlpath);
+  // console.log(graphqlpath);
   const cfReq = await fetch(graphqlpath)
     .then((response) => response.json())
     .then((contentfragment) => {
