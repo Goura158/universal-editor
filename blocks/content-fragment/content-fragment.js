bd1b64195456aac9a92faf6a191765fed7346399
Goura158 <130237259+Goura158@users.noreply.github.com>
2025-05-29 08:37:06 -0400

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -14,34 +14,23 @@ import {
 } from '../../scripts/aem.js';
 
 /**
- * Loads a fragment.
- * @param {string} path The path to the fragment
- * @returns {HTMLElement} The root element of the fragment
+ *
+ * @param {Element} block
  */
-export async function loadFragment(path) {
-  const aemauthorurl = getMetadata('authorurl') || '';
-  if (path && path.startsWith('/')) {
-    // eslint-disable-next-line no-param-reassign
-    // path = path.replace(/(\.plain)?\.html/, '');
-    const cfapipath = `${aemauthorurl}/graphql/execute.json/universal-editor-standard-site/text`;
-    const resp = await fetch(`${cfapipath}`)
-      .then((response) => response.json())
-      .then((contentfragment) => {
-        let textcf = '';
-        if (contentfragment.data) {
-          textcf = contentfragment.data[Object.keys(contentfragment.data)[0]].item;
-        }
-        return textcf;
-      });
-    console.log(resp);
-  }
-  return null;
-}
-
 export default async function decorate(block) {
-	const graphqlpath = `https://author-p14733-e1160558.adobeaemcloud.com/graphql/execute.json/universal-editor-standard-site/text`;
-	const graphqlpathresult = await loadFragment(`graphqlpath`);
-	console.log(graphqlpathresult);
+	const aemauthorurl = getMetadata('authorurl') || '';
+	const persistedquery = '/graphql/execute.json/universal-editor-standard-site/text';
+	const graphqlpath = `${aemauthorurl}${persistedquery}`;
+	const cfReq = await fetch(url, options)
+    .then((response) => response.json())
+    .then((contentfragment) => {
+      let offer = '';
+      if (contentfragment.data) {
+        offer = contentfragment.data[Object.keys(contentfragment.data)[0]].item;
+      }
+      return offer;
+    });
+	console.log(cfReq);
 	// const path = link ? link.getAttribute('href') : block.textContent.trim();
 	// const cftext = document.createElement('div');
   // while (fragment.title) cftext.append(fragment.title);
