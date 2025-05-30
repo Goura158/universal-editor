bd870861b5800f2d31ffd681e89c57c11196a45a
Goura158 <130237259+Goura158@users.noreply.github.com>
2025-05-30 12:28:47 -0400

    updating folder name

---
+++ blocks/content-fragment1/content-fragment1.js
@@ -0,0 +1,44 @@
+/*
+ * Fragment Block
+ * Include content on a page as a fragment.
+ * https://www.aem.live/developer/block-collection/fragment
+ */
+
+/**
+* import {
+* decorateMain,
+* } from '../../scripts/scripts.js';
+*/
+
+/**
+ *
+ * @param {Element} block
+ */
+export default async function decorate(block) {
+  block.innerHTML = '';
+  const aemauthorurl = 'https://author-p14733-e1160558.adobeaemcloud.com';
+  console.log('author url ', aemauthorurl);
+  const persistedquery = '/graphql/execute.json/universal-editor-standard-site/text';
+  const graphqlpath = `${aemauthorurl}${persistedquery}`;
+  // console.log(graphqlpath);
+  const cfReq = await fetch(graphqlpath)
+    .then((response) => response.json())
+    .then((contentfragment) => {
+      let offer = '';
+      if (contentfragment.data) {
+        console.log('CF data ', contentfragment.data);
+        offer = contentfragment.data[Object.keys(contentfragment.data)[0]].item;
+      }
+      return offer;
+    });
+  console.log(cfReq);
+  block.setAttribute('data-aue-type', 'container');
+  block.innerHTML = `
+  <div class='banner-content block' data-aue-label="offer content fragment" data-aue-type="reference" data-aue-filter="cf">
+    <p data-aue-prop="pretitle" data-aue-label="pretitle" data-aue-type="text" class='pretitle'>Today's Headline</p>
+  </div>`;
+  // const path = link ? link.getAttribute('href') : block.textContent.trim();
+  // const cftext = document.createElement('div');
+  // while (cfReq.title) cftext.append(cfReq.title);
+  // block.append(cftext);
+}
