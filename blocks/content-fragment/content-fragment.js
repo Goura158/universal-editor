9b125ec94aeae4effe28d3d3a03d7d4e30d8de8c
Goura158 <130237259+Goura158@users.noreply.github.com>
2025-06-17 10:44:11 -0400

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -42,7 +42,7 @@ export default async function decorate(block) {
   console.log('cf path ', path);
   const aemauthorurl = 'https://author-p14733-e1160558.adobeaemcloud.com';
   console.log('author url ', aemauthorurl);
-  const persistedquery = '/graphql/execute.json/universal-editor-standard-site/text';
+  const persistedquery = '/content/dam/universal-editor-standard-site/cf/headline';
   const graphqlpath = `${aemauthorurl}${persistedquery}`;
   console.log(graphqlpath);
   const cfReq = await fetch(graphqlpath)
