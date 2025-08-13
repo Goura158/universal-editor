2e8de4a1e2b8ca9954873e9a9a7659141f010046
Goura158 <130237259+Goura158@users.noreply.github.com>
2025-08-13 10:07:22 -0400

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -155,7 +155,8 @@ export default async function decorate(block) {
         const newText = block.querySelector('.editable-text').innerText.trim();
         console.log('newText of content fragment ', newText);
         // Replace values
-        obj.fields[0].values = newText;
+        // obj.fields[0].values = newText;
+        obj.elements.title.value = newText;
         console.log('new obj ', obj);
         try {
           // await updateContentFragment(cleanedFragmentPath, newText);
